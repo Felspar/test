09fac974139fcb1344cf8ec295d1575ae2afe982
Kirit Sælensminde <kirit@kirit.com>
2020-07-21 14:29:43 +0700

    Capture the correct source location when used according to the standard.

--- include/felspar/test/source.hpp
+++ include/felspar/test/source.hpp
@@ -23,18 +23,26 @@ namespace felspar {
 
 
     struct source_location {
-        static source_location current() { return {}; }
+        static source_location
+                current(char const *f = __builtin_FILE(),
+                        char const *fn = __builtin_FUNCTION(),
+                        unsigned l = __builtin_LINE(),
+                        unsigned c = __builtin_COLUMN()) {
+            return {f, fn, l, c};
+        }
 
         auto line() const { return l; }
         auto column() const { return c; }
         auto file_name() const { return f; }
         auto function_name() const { return fn; }
 
-    private:
-        char const *f = __builtin_FILE();
-        char const *fn = __builtin_FUNCTION();
-        unsigned l = __builtin_LINE();
-        unsigned c = __builtin_COLUMN();
+      private:
+        source_location(char const *f, char const *fn, unsigned l, unsigned c)
+        : f{f}, fn{fn}, l{l}, c{c} {}
+        char const *f = {};
+        char const *fn = {};
+        unsigned l = {};
+        unsigned c = {};
     };
 
 
