c054148849d81535a2a750e72e2c756bcb47da83
Kirit Sælensminde <kirit@kirit.com>
2021-10-23 14:34:43 +0700

    Add constexpr and noexcept as required

--- include/felspar/test/source.hpp
+++ include/felspar/test/source.hpp
@@ -23,21 +23,23 @@ namespace felspar {
 
 
     struct source_location {
-        static source_location
+        /// Can't be `consteval` due to clang bug
+        static /*consteval*/ constexpr source_location
                 current(char const *f = __builtin_FILE(),
                         char const *fn = __builtin_FUNCTION(),
                         unsigned l = __builtin_LINE(),
-                        unsigned c = __builtin_COLUMN()) {
+                        unsigned c = __builtin_COLUMN()) noexcept {
             return {f, fn, l, c};
         }
 
-        auto line() const { return l; }
-        auto column() const { return c; }
-        auto file_name() const { return f; }
-        auto function_name() const { return fn; }
+        constexpr auto line() const noexcept { return l; }
+        constexpr auto column() const noexcept { return c; }
+        constexpr auto file_name() const noexcept { return f; }
+        constexpr auto function_name() const noexcept { return fn; }
 
       private:
-        source_location(char const *f, char const *fn, unsigned l, unsigned c)
+        constexpr source_location(
+                char const *f, char const *fn, unsigned l, unsigned c)
         : f{f}, fn{fn}, l{l}, c{c} {}
         char const *f = {};
         char const *fn = {};
