cd8f0f25788dcf7ec407553d568956cb58dc268d
Kirit Sælensminde <kirit@kirit.com>
2022-10-03 20:47:39 +0700

    Fix up source_location argument passing in test checkers.

--- include/felspar/test/check.hpp
+++ include/felspar/test/check.hpp
@@ -23,8 +23,8 @@ namespace felspar::test {
         source_location source;
 
         template<typename X>
-        checks(injected const &c, X &&v, source_location l)
-        : check{c}, value{std::forward<X>(v)}, source{std::move(l)} {}
+        checks(injected const &c, X &&v, source_location const &l)
+        : check{c}, value{std::forward<X>(v)}, source{l} {}
 
         template<typename C>
         auto report(bool passed, std::string_view op, C const &c) const {
@@ -63,7 +63,7 @@ namespace felspar::test {
         template<typename E>
         auto
                 throws(E const &v,
-                       source_location loc = source_location::current()) const;
+                       source_location const &loc = source_location::current()) const;
         template<typename E>
         auto throws_type() const {
             bool passed{false};
@@ -90,21 +90,21 @@ namespace felspar::test {
     struct injected {
         template<testable_value V>
         auto operator()(
-                V &&v, source_location loc = source_location::current()) const {
-            return checks<V>{*this, std::forward<V>(v), std::move(loc)};
+                V &&v, source_location const &loc = source_location::current()) const {
+            return checks<V>{*this, std::forward<V>(v), loc};
         }
     };
 
 
     template<typename V>
     template<typename E>
-    inline auto checks<V>::throws(E const &v, source_location loc) const {
+    inline auto checks<V>::throws(E const &v, source_location const &loc) const {
         try {
             value();
         } catch (E &e) {
             std::string_view e_what = e.what();
             std::string_view v_what = v.what();
-            check(e_what.substr(0, e_what.find('\n')), std::move(loc))
+            check(e_what.substr(0, e_what.find('\n')), loc)
                     == v_what.substr(0, v_what.find('\n'));
             return std::current_exception();
         }
