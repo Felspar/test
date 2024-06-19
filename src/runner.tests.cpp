1af1e4875a8ac5b8498a013ebacaf0bb4b735e6a
Kirit Sælensminde <kirit@felspar.com>
2024-06-19 13:42:12 +0700

    clang-format

--- src/runner.tests.cpp
+++ src/runner.tests.cpp
@@ -4,24 +4,29 @@
 namespace {
 
 
-    auto const format = felspar::testsuite("format")
+    auto const format =
+            felspar::testsuite("format")
 
-                                .test("no extra",
-                                      [](auto check) {
-                                          check(felspar::test::format_failure_message("filename.cpp", 10, 4, "test")) ==
-        "filename.cpp:10:4: Error";
-                                      })
+                    .test("no extra",
+                          [](auto check) {
+                              check(felspar::test::format_failure_message(
+                                      "filename.cpp", 10, 4, "test"))
+                                      == "filename.cpp:10:4: Error";
+                          })
 
-                                .test("unary check",
-                                      [](auto check) {
-                                          check(felspar::test::format_failure_message("filename.cpp", 10, 4, "test", "value1")) ==
-        "filename.cpp:10:4: Error\n    check(value1) test";
-                                      })
+                    .test("unary check",
+                          [](auto check) {
+                              check(felspar::test::format_failure_message(
+                                      "filename.cpp", 10, 4, "test", "value1"))
+                                      == "filename.cpp:10:4: Error\n    check(value1) test";
+                          })
 
-                                .test("binary check", [](auto check) {
-                                    check(felspar::test::format_failure_message("filename.cpp", 10, 4, "test", "value1", "value2" )) ==
-        "filename.cpp:10:4: Error\n    check(value1) test value2";
-                                });
+                    .test("binary check", [](auto check) {
+                        check(felspar::test::format_failure_message(
+                                "filename.cpp", 10, 4, "test", "value1",
+                                "value2"))
+                                == "filename.cpp:10:4: Error\n    check(value1) test value2";
+                    });
 
 
 }
