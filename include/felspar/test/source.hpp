67f1fe3c8205e2062a244b4edbad33d069a941ae
Kirit Sælensminde <kirit@felspar.com>
2025-11-07 14:42:40 +0700

    Remove all shims for `std::source_location`

--- include/felspar/test/source.hpp
+++ include/felspar/test/source.hpp
@@ -1,9 +1,6 @@
 #pragma once
 
 
-#if __has_include(<source_location>)
-
-
 #include <source_location>
 
 
@@ -14,56 +11,3 @@ namespace felspar {
 
 
 }
-
-
-#elif __has_include(<experimental/source_location>)
-
-
-#include <experimental/source_location>
-
-
-namespace felspar {
-
-
-    using source_location = std::experimental::source_location;
-
-
-}
-
-
-#else
-
-
-namespace felspar {
-
-
-    struct source_location {
-        /// Can't be `consteval` due to clang bug
-        static /*consteval*/ constexpr source_location
-                current(char const *f = __builtin_FILE(),
-                        char const *fn = __builtin_FUNCTION(),
-                        unsigned l = __builtin_LINE(),
-                        unsigned c = __builtin_COLUMN()) noexcept {
-            return {f, fn, l, c};
-        }
-
-        constexpr auto line() const noexcept { return l; }
-        constexpr auto column() const noexcept { return c; }
-        constexpr auto file_name() const noexcept { return f; }
-        constexpr auto function_name() const noexcept { return fn; }
-
-      private:
-        constexpr source_location(
-                char const *f, char const *fn, unsigned l, unsigned c)
-        : f{f}, fn{fn}, l{l}, c{c} {}
-        char const *f = {};
-        char const *fn = {};
-        unsigned l = {};
-        unsigned c = {};
-    };
-
-
-}
-
-
-#endif
